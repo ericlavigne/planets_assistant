23935765a1d158177a24bb7c269456329b47b111
Eric Lavigne <lavigne.eric@gmail.com>
2014-07-20 20:14:02 -0700

    load turn/rst, calculate max factories/mines/defense

--- src/vgap/core.clj
+++ src/vgap/core.clj
@@ -21,6 +21,14 @@
                                                 "scope" %}})))
           ["0" "1"]))
 
+(defn game-loadturn [game-id apikey]
+  (json/read-str
+    (:body (client/get "http://api.planets.nu/game/loadturn"
+                       {:query-params {"gameid" game-id "apikey" apikey}}))))
+
+(defn game-loadrst [game-id apikey]
+  ((game-loadturn game-id apikey) "rst"))
+
 (defn ask [q]
   "Ask a question, return an answer"
   (println (str q ":"))
@@ -30,6 +38,28 @@
   (println (join " - " cols))
   (doseq [row rows]
     (println (join " - " (map row cols)))))
+
+(defn rst-myplanets [rst]
+  "Extract my planets from rst"
+  (let [my-id (get-in rst ["player" "id"])]
+     (filter #(= my-id (% "ownerid")) (rst "planets"))))
+
+(defn clans-max-mines [clans]
+   (if (<= clans 200) clans
+       (Math/round (Math/floor (+ 200 (Math/sqrt (- clans 200)))))))
+(defn clans-max-factories [clans]
+   (if (<= clans 100) clans
+       (Math/round (Math/floor (+ 100 (Math/sqrt (- clans 100)))))))
+(defn clans-max-defense [clans]
+   (if (<= clans 50) clans
+       (Math/round (Math/floor (+ 50 (Math/sqrt (- clans 50)))))))
+
+(defn planet-max-mines [planet]
+  (clans-max-mines (planet "clans")))
+(defn planet-max-factories [planet]
+  (clans-max-factories (planet "clans")))
+(defn planet-max-defense [planet]
+  (clans-max-defense (planet "clans")))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -42,5 +72,13 @@
         (do (println "apikey is" apikey)
             (print-grid (user-games u)
                         ["id" "name" "turn" "timetohost"])
+            (let [gameid (ask "Choose a game")
+                  turn (game-loadturn gameid apikey)
+                  rst (turn "rst")]
+              (println "In game" ((rst "game") "name")
+                       "you are playing race" ((rst "player") "raceid"))
+              (print-grid (rst-myplanets rst) ["id" "name"])
+              (print-grid (rst-myplanets rst) (keys (first (rst-myplanets rst))))
+            )
             (println "Bye!")))))
 
--- test/vgap/core_test.clj
+++ test/vgap/core_test.clj
@@ -2,6 +2,37 @@
   (:require [clojure.test :refer :all]
             [vgap.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def homeworld
+     {"id" 24, "name" "Kaye's World", "temp" 50, "ownerid" 6, "x" 1845, "y" 1524,
+
+      "clans" 26250, "colchange" 0, "colonisttaxrate" 0, "colonisthappypoints" 100, "colhappychange" 7,
+
+      "nativeclans" 0, "nativechange" 0, "nativetaxrate" 0, "nativehappypoints" 85, "nativehappychange" 4,
+      "nativeracename" "none", "nativetype" 0,
+      "nativegovernment" 0, "nativegovernmentname" "?", "nativetaxvalue" 20,
+
+      "factories" 100, "mines" 100, "defense" 20,
+      "builtfactories" 0, "builtmines" 0, "builtdefense" 0,
+      "targetfactories" 0, "targetmines" 0, "targetdefense" 0,
+
+      "supplies" 650, "suppliessold" 0, "checksupplies" 870,
+      "megacredits" 6935, "checkmegacredits" 15635,
+
+      "neutronium" 450,"duranium" 395, "tritanium" 663, "molybdenum" 554,
+      "groundneutronium" 10949, "groundduranium" 2814, "groundtritanium" 2435, "groundmolybdenum" 1451,
+      "densityneutronium" 50, "densityduranium" 15, "densitytritanium" 20, "densitymolybdenum" 95,
+      "checkneutronium" 550, "checkduranium" 495, "checktritanium" 723, "checkmolybdenum" 594,
+      "totalneutronium" 0, "totalduranium" 0, "totaltritanium" 0, "totalmolybdenum" 0,
+
+      "friendlycode" "100", "flag" 1, "readystatus" 0, "buildingstarbase" false,
+
+      "debrisdisk" 0, "infoturn" 2,
+      "img" "http://library.vgaplanets.nu/planets/150.png"})
+
+(deftest govern-homeworld
+  (testing "Construction and taxes for first turn"
+     (is (= 361 (planet-max-mines homeworld)))
+     (is (= 261 (planet-max-factories homeworld)))
+     (is (= 211 (planet-max-defense homeworld)))
+  ))
+
