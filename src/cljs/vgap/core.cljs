cf3edb2f227b81d86df1c2c024bd13fd21cf99a0
Eric Lavigne <lavigne.eric@gmail.com>
2015-03-29 19:20:38 -0700

    Remove routing - this app doesn't need bookmarkable URLs

--- src/cljs/vgap/core.cljs
+++ src/cljs/vgap/core.cljs
@@ -20,16 +20,8 @@
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to vgap"]
-   [:div [:a {:href "#/about"} "go to about page"]]
-   [:div [:a {:href "#/login"} "go to login page"]]])
-
-(defn about-page []
-  [:div [:h2 "About vgap"]
-   [:div [:a {:href "#/"} "go to the home page"]]])
-
-(defn current-page []
-  [:div [(session/get :current-page)]])
+  [:div [:h2 "Welcome to vgap " (:username @user)]
+   [:div [:a {:href "#" :on-click #(reset! user {})} "Logout"]]])
 
 (defn login-handler [username password res]
   (.log js/console (str res))
@@ -68,26 +60,17 @@
                                                   (str "something bad happened: " status
                                                        " " status-text)))
                                           })))}]
-            [:p (str "Username is: " (:username @user))]
 ])))
 
-;; -------------------------
-;; Routes
-(secretary/set-config! :prefix "#")
-
-(secretary/defroute "/" []
-  (session/put! :current-page home-page))
-
-(secretary/defroute "/about" []
-  (session/put! :current-page about-page))
-
-(secretary/defroute "/login" []
-  (session/put! :current-page login-page))
+(defn main-page []
+  (if (:username @user)
+      [home-page]
+      [login-page]))
 
 ;; -------------------------
 ;; Initialize app
 (defn init! []
-  (reagent/render-component [current-page] (.getElementById js/document "app")))
+  (reagent/render-component [main-page] (.getElementById js/document "app")))
 
 ;; -------------------------
 ;; History
