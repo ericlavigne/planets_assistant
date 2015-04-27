747e4eea0ab16c0be5fcf0e68b987f94a36e4d1c
Eric Lavigne <lavigne.eric@gmail.com>
2015-04-27 04:30:35 -0700

    Fetch resign/drop/dead events for game

--- test/vgap/scores_test.clj
+++ test/vgap/scores_test.clj
@@ -15,6 +15,9 @@
     (let [pls-events (fetch-game-events 100282)
           expected-events [{:type :join :player-num 6 :account-name "ericlavigne" :account-id 20214 :turn 1}
                            {:type :join :player-num 2 :account-name "battle toast" :account-id 15059 :turn 43}
+                           {:type :resign :player-num 1 :account-name "macros the black" :account-id 18615 :turn 101}
+                           {:type :drop :player-num 4 :account-name "kokunai" :account-id 4784 :turn 32}
+                           {:type :dead :player-num 9 :turn 41}
                            ; {:type :win :player-num 10 :account-name "yahoud" :account-id 20989 :turn 103}
                            ]]
        (doseq [e expected-events]
